ba7cc32816cbd031fca9fd4e89ae28b9ca959299
Alison Shin <alisonyshin@gmail.com>
2018-07-30 08:36:47 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(phrase)
   word_array = phrase.split(" ")
   new_array =[]
   word_array.each {|word| new_array << word.reverse()}
-  puts new_array
+  return new_array
 end
 
 phrase = "Hello there, and how are you?"
